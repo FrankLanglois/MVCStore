2101eb301de2cc778924447c65592796491748c0
Frank <franklanglois@gmail.com>
2017-09-19 11:56:28 -0400

    Remove password restrictions. Added Store controller and View.

---
+++ MVCStore/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStore.Controllers
+{
+    [Authorize]
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Store( string name)
+        {
+            return HttpUtility.HtmlEncode( $"Hello {name}!");
+        }
+    }
+}
